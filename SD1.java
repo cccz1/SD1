11fe25324284da9675a0c6a65233aa0b4a2edfda
cccz1 <45119376+cccz1@users.noreply.github.com>
2022-02-04 16:29:11 -0800

    1

---
+++ SD1.java
@@ -0,0 +1,5 @@
+public class SD1{
+    public static boolean LargerThan5(int n){
+        return n>5;
+    }
+}
---
+++ SD1T.java
@@ -0,0 +1,10 @@
+import static org.junit.Assert.*;
+import org.junit.*;
+
+public class SD1T{
+    @Test
+    public void compare(){
+        assertEquals(true, SD1.LargerThan5(1));
+
+    }
+}
